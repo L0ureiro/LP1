6f6dc6b3e1fb5b676c79b01bae67e84a32138828
L0ureiro <loureirolucas98@gmail.com>
2022-08-30 12:12:27 -0300

    Passagem do código com cmake para /aula_03

--- aula_02/CMakeFiles/modularizacao.dir/conv.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/modularizacao.dir/conv.cpp.o: \
- /home/lucas.loureiro.117/Área\ de\ Trabalho/Cpp/LP1/aula_02/conv.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas.loureiro.117/Área\ de\ Trabalho/Cpp/LP1/aula_02/conv.h
--- aula_02/CMakeFiles/modularizacao.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for modularizacao.
-# This may be replaced when dependencies are built.
