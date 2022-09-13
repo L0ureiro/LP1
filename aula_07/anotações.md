bc0de125dd27937d938adb632b5b22099e6773a0
L0ureiro <loureirolucas98@gmail.com>
2022-09-13 12:26:58 -0300

    commit primeiros arquivos para exercicio da aula 07

---
+++ aula_07/anotações.md
@@ -0,0 +1,8 @@
+# Anotações - Aula 07
+
+## Programção Orientada a Objetos (POO)
+
+    Entidade -> Classe
+    Característica -> Atributo
+    Ações -> Métodos
+
---
+++ aula_07/include/aluno.h
@@ -0,0 +1,21 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#include <string>
+
+
+class Aluno { 
+private:
+    std::string Nome;
+    std::string Cpf;
+    int Idade;
+    int Periodo;
+    char MatricRegul;
+
+public:
+    
+};
+
+
+
+#endif
